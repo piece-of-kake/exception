e4e1f695d2c73245ee737304551c6e0249d2890e
Sasa Andjelic <sasasmail@gmail.com>
2021-10-26 08:14:13 +0200

    Forbidden exception accepting message;

--- src/ClientError/ForbiddenException.php
+++ src/ClientError/ForbiddenException.php
@@ -4,8 +4,14 @@ namespace PoK\Exception\ClientError;
 
 class ForbiddenException extends \Exception
 {
-    public function __construct(\Throwable $previous = NULL) 
+    public function __construct(string $message = null, \Throwable $previous = NULL)
     {
-        parent::__construct('FORBIDDEN', 403, $previous);
+        parent::__construct(
+            $message
+                ? $message
+                : 'FORBIDDEN',
+            403,
+            $previous
+        );
     }
 }
